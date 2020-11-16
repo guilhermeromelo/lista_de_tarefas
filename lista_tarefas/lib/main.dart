be9b490a67bb43363508d0e4f4197fd59a8139c0
Guilherme Melo <guilhermeromelo@gmail.com>
2020-11-16 01:23:47 -0300

    Funções gravação e leitura arquivo Prontas e Início da Tela Principal.
    Foi feito as funções de pegar o arquivo no sistema, ler ou gravar e também foi feito a primeira parte da tela, onde ficam um campo e um botão.

--- lista_tarefas/lib/main.dart
+++ lista_tarefas/lib/main.dart
@@ -1,7 +1,13 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:async';
 
 void main() {
-  runApp(Home());
+  runApp(MaterialApp(home: Home()));
 }
 
 class Home extends StatefulWidget {
@@ -9,15 +15,86 @@ class Home extends StatefulWidget {
   _HomeState createState() => _HomeState();
 }
 
+final taskController = TextEditingController();
+
+void _taskChanged(String text) {}
+
 class _HomeState extends State<Home> {
+  List _toDoList = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(title: Text("Lista de Tarefas", style: TextStyle(color: Colors.white, fontSize: 20),),centerTitle: true,),
-      body: Container(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: Text(
+            "Lista de Tarefas",
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          ),
+          centerTitle: true,
+        ),
+        body: Padding(
+            padding: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+            child: Column(children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    flex: 72,
+                    child: buildTextField(
+                        "Nova Tarefa", "", taskController, _taskChanged),
+                  ),
+                  Expanded(
+                    flex: 3,
+                    child: Container(),
+                  ),
+                  Expanded(
+                    flex: 25,
+                    child: RaisedButton(
+                      color: Colors.blueAccent,
+                      textColor: Colors.white,
+                      child: Text(
+                        "ADD",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      onPressed: () {},
+                    ),
+                  )
+                ],
+              )
+            ])));
+  }
 
-      ),
+  Future<File> _getFile() async {
+    final directory = await getApplicationDocumentsDirectory();
+    return File("${directory.path}/data.json");
+  }
+
+  Future<File> _saveData() async {
+    String Data = json.encode(_toDoList);
+    final file = await _getFile();
+    return file.writeAsString(Data);
+  }
+
+  Future<String> _readData() async {
+    try {
+      final file = await _getFile();
+      return file.readAsString();
+    } catch (e) {
+      return null;
+    }
+  }
+
+  Widget buildTextField(String label, String prefix,
+      TextEditingController controller, Function function) {
+    return TextField(
+      controller: controller,
+      decoration: InputDecoration(
+          labelText: label,
+          labelStyle: TextStyle(color: Colors.blueAccent),
+          border: OutlineInputBorder(),
+          prefixText: prefix),
+      style: TextStyle(color: Colors.black, fontSize: 25),
+      onChanged: function,
     );
   }
 }
